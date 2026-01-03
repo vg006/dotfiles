26b5cf61b5b28ea0d4d9ede1d09a7582584f31c3
vg006 <devvg006@gmail.com>
2026-01-03 11:24:41 +0530

    ♻ chore: Update PATH, fish variables and btop
    Signed-off-by: vg006 <devvg006@gmail.com>

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,5 @@
 export BIN_GO="$HOME/go/bin"
 export BIN_RS="$HOME/.cargo/bin"
 export EDITOR="hx"
-export PATH="$PATH:$HOME/.local/bin:$BIN_GO:$BIN_RS:$HOME/.fex/bin"
+export PATH="$PATH:$HOME/.local/bin:$BIN_GO:$BIN_RS:$HOME/.fex/bin:$HOME:/.opencode/bin"
 export FEX_DEFAULT_COMMAND="fex --time-type modified"
